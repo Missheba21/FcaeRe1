6dc35aaef03dc39cfaa4ee4dabed0c120c59667d
Missheba21 <157479072+Missheba21@users.noreply.github.com>
2024-02-05 20:29:43 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='YourPackageName',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'pandas==1.3.3',
+        # Add other dependencies as needed
+    ],
+)
